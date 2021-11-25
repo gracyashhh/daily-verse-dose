09575e22162cba88c0fe21a1ed1e55eaba99cee7
aishwaryam18 <aishwaryam18@karunya.edu.in>
2021-11-25 15:09:52 +0530

    clock changed

--- clock.py
+++ clock.py
@@ -5,6 +5,6 @@ from main import sending
 sched = BlockingScheduler()
 
 # Schedule job_function to be called every two hours
-sched.add_job(sending, 'interval', seconds=10)
+sched.add_job(sending, 'interval', seconds=10000)
 
 sched.start()
