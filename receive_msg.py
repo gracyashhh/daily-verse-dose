cdd2cba79e5f91eeadbfcf3b96e86c9fe8469deb
aishwaryam18 <aishwaryam18@karunya.edu.in>
2021-11-26 16:06:20 +0530

    added basic website layout

--- receive_msg.py
+++ receive_msg.py
@@ -1,10 +1,10 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 from twilio.twiml.messaging_response import MessagingResponse
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello Aish'
+    return render_template('index.html')
 
 @app.route('/sms',methods=['GET','POST'])
 def msg_reply():
